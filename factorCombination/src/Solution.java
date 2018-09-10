d0031fa72b3c84c9137caf005072c647b824f23f
xrlu0929 <xl2474@tc.columbia.edu>
2018-09-10 17:52:27 -0400

    factor

---
+++ factorCombination/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinations(int target) {
+        // Write your solution here
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<>();
+
+        factor(1, 2, target, list, result);
+        return result;
+    }
+
+    private void factor(int product, int start, int n, List<Integer>list, List<List<Integer>>result) {
+
+        if(start > n || product > n) {
+            return;
+        }
+
+        if (product == n) {
+            // hard point 1
+            ArrayList<Integer> t = new ArrayList<Integer>(list);
+
+            result.add(t);
+            return;
+        }
+
+        for(int i = start; i < n; i++) {
+            // don't have to be prime number
+            if(i * product > n) {
+                break;
+            }
+            if (n % i == 0) {
+                // if 整除
+                list.add(i);
+                factor(i* product, i, n, list, result);
+
+                // hard point 2
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
